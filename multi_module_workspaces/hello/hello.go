6455b24929ff3dab498d115d12c854769c96a73e
yosnak13 <yosnak13@gmail.com>
2022-10-26 07:44:19 +0900

    toupper

--- multi_module_workspaces/hello/hello.go
+++ multi_module_workspaces/hello/hello.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"golang.org/x/example/stringutil"
+    "fmt"
+
+    "golang.org/x/example/stringutil"
 )
 
 func main() {
-	fmt.Println(stringutil.Reverse("Hello"))
+    fmt.Println(stringutil.ToUpper("Hello"))
 }
