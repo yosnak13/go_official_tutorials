89b7596cde25cb19d50083a8fa42cd74e08239d0
yosnak13 <yosnak13@gmail.com>
2022-10-27 08:10:19 +0900

    db接続

--- data-access/main.go
+++ data-access/main.go
@@ -1,3 +1,33 @@
 package main
 
-import "github.com/go-sql-driver/mysql"
+import (
+	"github.com/go-sql-driver/mysql"
+	"fmt"
+	"log"
+	"os"
+)
+
+var db *sql.DB
+
+func main() {
+	cfg := mysql.Config{
+		User: os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net: "tcp",
+		Addr: "127.0.0.1:3306",
+		DBName: "recordings",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+
+	fmt.Println("Connected!")
+}
